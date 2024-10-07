3676145f5f30fee457809c2540bee6f062c75263
Vera Knyazeva <verashibanova@yandex.ru>
2024-10-07 16:51:40 +0500

    правки

--- create_orders.sql
+++ create_orders.sql
@@ -1,7 +1,8 @@
-CREATE TABLE ORDERS(
-                       id serial PRIMARY KEY,
-                       date DATE,
-                       customer_id BIGINT,
-                       product_name VARCHAR(255),
-                       amount INT
+CREATE TABLE ORDERS (
+                        id serial PRIMARY KEY,
+                        date DATE,
+                        customer_id INT,
+                        product_name VARCHAR(255),
+                        amount INT,
+                        FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id)
 );
